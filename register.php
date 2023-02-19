54119436f5484d12b2ca1c77031e08a3207d1c5e
Roberto Gentilini <roby.gent@gmail.com>
2023-02-19 15:57:16 +0100

    a

--- register.php
+++ register.php
@@ -45,6 +45,7 @@
 	} catch (phpmailerException $e) {
 		echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
 	} catch (Exception $e) {
-		echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+		echo "Email not sent! {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
 	}
+	
 ?>
