f6c62f1974de0538c36fc37fa6c33a7eb8cc0104
Roberto Gentilini <roby.gent@gmail.com>
2023-02-19 15:31:26 +0100

    a

--- html/register.php
+++ html/register.php
@@ -1,6 +1,10 @@
 <?php include "../inc/dbinfo.inc"; ?>
 <?php
-
+	// Import PHPMailer classes into the global namespace
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	require '/usr/local/bin/vendor/autoload.php';
+	
 	// Try and connect using the info above.
 	$con = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
 	if (mysqli_connect_errno()) {
@@ -42,6 +46,7 @@
 		} else {
 			// Username doesn't exist, insert new account
 			if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)')) {
+				
 				// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
 				$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 				$uniqid = uniqid();
@@ -52,8 +57,49 @@
 				$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 				// Update the activation variable below
 				$activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $_POST['email'] . '&code=' . $uniqid;
-				$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
-				mail($_POST['email'], $subject, $message, $headers);
+				
+				$sender = 'nuvolacloudstorage@gmail.com';
+				$senderName = 'No Reply';
+				$recipient = 'roby.gent@gmail.com';
+				$usernameSmtp = 'AKIA5MFAAP5YNACHDRDI';
+				$passwordSmtp = 'BFY2mgH6631k5IXd+fTdzttK9lNsofJ5O0KgyM+s595n';
+				$configurationSet = '';
+				$host = 'email-smtp.eu-central-1.amazonaws.com';
+				$port = 587;
+				$subject = 'User code';
+				$bodyText =  "Nuvola Cloud Storage\r\nPlease click the following link to activate your account: " . $activate_link;
+				$bodyHtml = '<h1>Nuvola Cloud Storage</h1>
+					<p>Please click the following link to activate your account: 
+					<a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+					
+				try {
+					// Specify the SMTP settings.
+					$mail->isSMTP();
+					$mail->setFrom($sender, $senderName);
+					$mail->Username   = $usernameSmtp;
+					$mail->Password   = $passwordSmtp;
+					$mail->Host       = $host;
+					$mail->Port       = $port;
+					$mail->SMTPAuth   = true;
+					$mail->SMTPSecure = 'tls';
+					$mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+
+					// Specify the message recipients.
+					$mail->addAddress($recipient);
+					// You can also add CC, BCC, and additional To recipients here.
+
+					// Specify the content of the message.
+					$mail->isHTML(true);
+					$mail->Subject    = $subject;
+					$mail->Body       = $bodyHtml;
+					$mail->AltBody    = $bodyText;
+					$mail->Send();
+					echo "Email sent!" , PHP_EOL;
+				} catch (phpmailerException $e) {
+					echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+				} catch (Exception $e) {
+					echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+				}
 				echo 'Please check your email to activate your account!';
 			} else {
 				// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
