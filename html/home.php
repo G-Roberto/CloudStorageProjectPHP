d9f2120077c189075c9bb09b1597ca55404a799a
Roberto Gentilini <roby.gent@gmail.com>
2023-02-21 13:41:24 +0100

    a

--- html/home.php
+++ html/home.php
@@ -1,3 +1,4 @@
+<?php include "../inc/dbinfo.inc"; ?>
 <?php
 	// We need to use sessions, so you should always start sessions using the below code.
 	session_start();
@@ -8,16 +9,6 @@
 	}
 	
 	echo $_SESSION['username'];
-	
-	$con = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
-	if (mysqli_connect_errno()) {
-		// If there is an error with the connection, stop the script and display the error.
-		exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-	}
-	if ($account['activation_code'] != 'activated') {
-		header('Location: success.html');
-	}
-	$con->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
