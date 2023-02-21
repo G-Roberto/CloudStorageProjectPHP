c5df44ccf8f9aace06aa28d83f28a2688f626072
Roberto Gentilini <roby.gent@gmail.com>
2023-02-21 14:09:06 +0100

    a

--- html/home.php
+++ html/home.php
@@ -11,12 +11,13 @@
 	echo $_SESSION['name'];
 	
 	
+	
 	$con = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
 	if (mysqli_connect_errno()) {
 		// If there is an error with the connection, stop the script and display the error.
 		exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 	}
-	$sql="SELECT activation_code FROM accounts WHERE username='$_SESSION['name'];'";
+	$sql="SELECT activation_code FROM accounts WHERE username='Roberto'";
 	$activation = mysqli->query($sql);
 	if ($activation != 'activated') {
 		header('Location: success.html');
